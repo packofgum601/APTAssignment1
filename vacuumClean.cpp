bcfa6e9339f397b8ad8d02d9430c52e377ca920a
Joseph Packham <josephpackham@outlook.com>
2024-03-20 16:11:13 +1100

    start

---
+++ vacuumClean.cpp
@@ -0,0 +1,28 @@
+#include "game.h"
+#include "helper.h"
+
+using std::string;
+
+void showStudentInformation(string name, string id, string email){
+    std::cout << "------------------------------------------------\n";
+    std::cout << "Name: Joseph Packham \n";
+    std::cout << "Student ID: s38389789 \n";
+    std::cout << "Email: s3838978@student.rmit.edu.au \n";
+    std::cout << "------------------------------------------------\n";
+}
+
+int main()
+{
+    /**
+     * TODO: here's the main function. You can write the "main menu" loop/code
+     * here or you can make separate functions - up to you.
+     */
+    std::cout << "Welcome to Vacuum CLeaning Game!";
+    std::cout << "------------------------------------------------\n";
+    
+
+    std::cout << "Good bye!\n\n";
+
+    return EXIT_SUCCESS;
+}
+
