095692682f0aea322da967daf6c4b08b07cd5d95
Joseph Packham <s3838978@student.rmit.edu.au>
2024-03-22 10:47:59 +1100

    REQ1 finished

--- game.cpp
+++ game.cpp
@@ -20,8 +20,20 @@ void Game::start()
     std::cout << "load <g>\n";
     std::cout << "  g: number of the game board to load\n";
     std::cout << "init <x>,<y>,<direction>\n";
-    std::cout << "  x: horizontal position of the vacuum cleaner in the room (between 0 & 9)";
-    std::cout << "  y: vertical position of the vacuum cleaner in the room (between 0 & 9)";
+    std::cout << "  x: horizontal position of the vacuum cleaner in the room (between 0 & 9) \n";
+    std::cout << "  y: vertical position of the vacuum cleaner in the room (between 0 & 9) \n";
+    std::cout << "  direction: direction of the vacuum cleaner's movement (north, east, south, west) \n";
+    std::cout << "forward (or f) \n";
+    std::cout << "turn_left (or l) \n";
+    std::cout << "turn_right (or r) \n";
+    std::cout << "quit \n";
+    std::cout << "Press enter to continue ...";
+    std::cin.ignore(); 
+    std::cin.get();
+    
+
+    //https://stackoverflow.com/questions/27081195/trying-to-understand-stdcin-get
+    
 
 }
 
